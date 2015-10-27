a57decd87b90e401fc33cd417a20436ac2527a9d
Robert <rothattack@gmail.com>
2015-10-27 15:50:05 -0500

    added animation dependency

--- mod_leidos_ars_search/tmpl/default.php
+++ mod_leidos_ars_search/tmpl/default.php
@@ -1,5 +1,6 @@
 <?php defined('_JEXEC') or die; ?>
 <?php JHTML::stylesheet('modules/mod_leidos_ars_search/css/main.css'); ?>
+<?php JHTML::stylesheet('modules/mod_leidos_ars_search/css/bounce.animation.css'); ?>
 <?php JHTML::script('https://code.angularjs.org/1.4.7/angular.min.js'); ?>
 <?php JHTML::script('https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.15/angular-ui-router.js'); ?>
 <?php JHTML::script('modules/mod_leidos_ars_search/app/main.js'); ?>
