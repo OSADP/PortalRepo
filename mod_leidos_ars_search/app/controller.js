84cdd5413e32d4deff1f961abbb63620051bf266
Robert <rothattack@gmail.com>
2015-10-28 16:25:40 -0500

    we basically grab all apps and just work with that for now

--- mod_leidos_ars_search/app/controller.js
+++ mod_leidos_ars_search/app/controller.js
@@ -5,44 +5,44 @@
 * Description
 */
 angular.module('Leidos.OSADP.Akeeba.Application.Search')
-.controller('CategoriesCtrl', ['$scope', '$timeout', '$http', '$stateParams', 'categories', CategoriesList])
+.controller('CategoriesCtrl', ['$scope', '$timeout', '$http', '$stateParams', 'categories', 'items', CategoriesList])
 
-function CategoriesList ( $scope, $timeout, $http, $stateParams, categories ) {
+function CategoriesList ( $scope, $timeout, $http, $stateParams, categories, items ) {
 	$scope.showLoader = true;
-	$scope.categories = categories.data || {};
+	$scope.categories = categories.data || [];
+	$scope.items = items.data || [];
+	$scope.environments = ["-", "-", "linux", "osx", "apple", "windows", "android"];
 
-	$scope.categories.unshift({
-		title: 'All Applications',
-		active: true,
-		id: 'all'
-	});
+	if( $scope.categories.length > 0 ) {	
+		$scope.categories.unshift({
+			title: 'All Applications',
+			active: true,
+			id: 'all'
+		});
+	}
 
-	angular.forEach( categories.data, function( category ) {
+	angular.forEach( $scope.categories, function( category ) {
 		category.active = false;
-		if ( $stateParams.category == category.id ) {
-			$scope.currentCategory = category.title;
+		category.items = [];
+
+		if( category.id == 'all' ) {
+			category.items = $scope.items;
+		} else {
+			angular.forEach( $scope.items, function( item ) {
+				item.environment = $scope.environments[item.environments.split('"')[1]];
+				if( category.id == item.category_id ) {
+					category.items.push( item );
+				}
+			})
+		}
+
+		if ( $stateParams.categoryId == category.id ) {
+			$scope.currentCategory = category;
 			category.active = true;
 		}
 	})
 
-	$http.get('/osadp/leidos/custom/services/akeeba/releases')
-	.then( function( promise ) {
-		$timeout(function() {
-			$scope.showLoader = false;
-			$scope.releases = promise.data;
-			$scope.categories[0].num_releases = $scope.releases.length;
-			angular.forEach( $scope.categories, function( category, index ) {
-				if( index != 0 ) {
-					category.num_releases = 0;
-					angular.forEach( $scope.releases, function( release ) {
-						if( release.category_id == category.id) {
-							category.num_releases++;
-						}
-					})
-				}
-			})
-		}, 1000)
-	})
-
-
+	$timeout( function() {
+		$scope.showLoader = false;
+	}, 800)
 }
