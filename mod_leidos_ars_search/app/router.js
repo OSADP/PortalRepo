af79ef99d351be37b4b076600f9ce0258b847998
Robert <rothattack@gmail.com>
2015-10-28 16:25:01 -0500

    had to change path, but this is a better implementation

--- mod_leidos_ars_search/app/router.js
+++ mod_leidos_ars_search/app/router.js
@@ -7,12 +7,18 @@ angular.module('Leidos.OSADP.Akeeba.Application.Search')
 	$stateProvider
 	// State for Listing all users
 	.state('listAll', {
-		url: '/:category',
-		templateUrl: 'modules/mod_leidos_ars_search/app/partials/main.ng.html',
+		url: '/:categoryId',
+		templateUrl: '/osadp/modules/mod_leidos_ars_search/app/partials/main.ng.html',
 		controller: 'CategoriesCtrl',
 		resolve: {
 			categories: ['$http', function( $http ) {
 				return $http.get('/osadp/leidos/custom/services/akeeba/categories')
+					.then( function( promise ) {
+						return promise;
+					})
+			}],
+			items: ['$http', function( $http ) {
+				return $http.get('/osadp/leidos/custom/services/akeeba/items')
 					.then( function( promise ) {
 						return promise;
 					})
