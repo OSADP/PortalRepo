3108d5dd0e89756341eeb638064e5a697424ba67
Robert <rothattack@gmail.com>
2015-10-28 16:24:19 -0500

    quality of life changes

--- mod_leidos_ars_search/tmpl/default.php
+++ mod_leidos_ars_search/tmpl/default.php
@@ -1,12 +1,14 @@
-<?php defined('_JEXEC') or die; ?>
-<?php JHTML::stylesheet('modules/mod_leidos_ars_search/css/main.css'); ?>
-<?php JHTML::stylesheet('modules/mod_leidos_ars_search/css/bounce.animation.css'); ?>
-<?php JHTML::script('https://code.angularjs.org/1.4.7/angular.min.js'); ?>
-<?php JHTML::script('https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.15/angular-ui-router.js'); ?>
-<?php JHTML::script('modules/mod_leidos_ars_search/app/main.js'); ?>
-<?php JHTML::script('modules/mod_leidos_ars_search/app/controller.js'); ?>
-<?php JHTML::script('modules/mod_leidos_ars_search/app/router.js'); ?>
+<?php 
+defined('_JEXEC') or die;
 
+JHTML::stylesheet('modules/mod_leidos_ars_search/css/main.css'); 
+JHTML::stylesheet('modules/mod_leidos_ars_search/css/bounce.animation.css'); 
+JHTML::script('https://code.angularjs.org/1.4.7/angular.min.js'); 
+JHTML::script('https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.15/angular-ui-router.js'); 
+JHTML::script('modules/mod_leidos_ars_search/app/main.js'); 
+JHTML::script('modules/mod_leidos_ars_search/app/controller.js'); 
+JHTML::script('modules/mod_leidos_ars_search/app/router.js'); 
+?>
 
 
 <div id="arsSearch" class="container">
