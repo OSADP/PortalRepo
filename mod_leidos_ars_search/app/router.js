13c8446a3fdfa5e40994f7836848d777f0986f7b
Robert <rothattack@gmail.com>
2015-11-02 17:00:09 -0600

    added comments

--- mod_leidos_ars_search/app/router.js
+++ mod_leidos_ars_search/app/router.js
@@ -3,7 +3,7 @@
 angular.module('Leidos.OSADP.Akeeba.Application.Search')
 // router configuration for this module
 .config(['$urlRouterProvider', '$stateProvider', function( $urlRouterProvider, $stateProvider ) {
-	// build our routes
+	// build our routes with $stateProvider
 	$stateProvider
 	// State for Listing all users
 	.state('listAll', {
@@ -11,7 +11,7 @@ angular.module('Leidos.OSADP.Akeeba.Application.Search')
 		templateUrl: '/osadp/modules/mod_leidos_ars_search/app/partials/main.ng.html',
 		controller: 'AkeebaItemsCtrl'
 	})
-
+	// set default route, works specially on initial page load
 	$urlRouterProvider.otherwise('/all');
 
 }])
