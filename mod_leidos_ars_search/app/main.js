8ac7c19ae7842d7abf883dbd5f80670df5f4367b
Robert <rothattack@gmail.com>
2015-11-03 14:41:56 -0600

    added fast click for touch devices

--- mod_leidos_ars_search/app/main.js
+++ mod_leidos_ars_search/app/main.js
@@ -9,15 +9,17 @@
 *
 * Description
 * Main module for displaying Akeeba Release Systems'
-* applications as well as provide a search and sort function.
+* releases as well as provide a search and sort function.
 */
 angular.module('Leidos.OSADP.Akeeba.Application.Search', ['ui.router']);
 
-;(function(window, document, $, undefined) {
-
-	$(document).ready( function() {
-		var arsSearcher = $('#arsSearch');
-		angular.bootstrap( arsSearcher, ['Leidos.OSADP.Akeeba.Application.Search']);
-	});
-
-})(window, document, jQuery);
+;(function(window, document, $, FastClick, undefined) {
+  $(document).ready( function() {
+  	// FastClick removes the 300ms delay on touch devices for click events like links etc..
+    FastClick.attach(document.body);
+    // Since we're waiting for the PHP to load the DOM we need to bootstrap our Angular app
+    // in this manner instead of the usual ng-app attribute
+  	var arsSearcher = $('#arsSearch');
+   	angular.bootstrap( arsSearcher, ['Leidos.OSADP.Akeeba.Application.Search']);
+  });
+})(window, document, jQuery, FastClick);
