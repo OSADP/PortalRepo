b232450ecf8587ab38cd20190816eb76337e62f4
chuckerin <chuckerin@gmail.com>
2016-01-07 10:15:09 -0600

    fixed copy pasta, formatted

--- leidos/custom/services/services/akeeba.impl.php
+++ leidos/custom/services/services/akeeba.impl.php
@@ -57,7 +57,8 @@ class ArsService extends DBConfig {
 	// Get all the releases
 	function getAllReleases() {
 		// prepare the statement
-		$stmt = $this->db->query ( 'SELECT * FROM jos_ars_releases' );
+		$stmt = $this->db->query ( 
+			'SELECT * FROM jos_ars_releases' );
 
 		// put the results in an array
 		while ( $row = $stmt->fetch_array ( MYSQL_ASSOC ) ) {
@@ -73,89 +74,19 @@ class ArsService extends DBConfig {
 	function getRelease($releaseId) {
 		// prepare the statement
 		$stmt = $this->db->query ( 
-				'SELECT * FROM jos_ars_releases  
-				WHERE id=' . $releaseId );
+			'SELECT * FROM jos_ars_releases 
+			WHERE id=' . $releaseId );
 
 		// should only return one result.  put that row in a variable
 		while ( $row = $stmt->fetch_array ( MYSQL_ASSOC ) ) {
 			$rel = $row;
 		}
 				
-		// close out the statement and return the one category
+		// close out the statement and return the one release
 		$stmt->close ();
 		return $rel;
 	}
 	
-/*
-	// Get all the items
-	function getAllItems() {
-		// prepare the statement
-		$stmt = $this->db->query ( 'SELECT * FROM jos_ars_items' );
-
-		// put the results in an array
-		while ( $row = $stmt->fetch_array ( MYSQL_ASSOC ) ) {
-			$arrItems[] = $row;
-		}
-		
-		// close out the statement and return the array
-		$stmt->close ();
-		return $arrItems;
-	}
-	
-	function getItem($itemId) {
-		// prepare the statement
-		$stmt = $this->db->query ( 
-				'SELECT id, release_id, title, alias, description, type, 
-					filename, url, updatestream, md5, sha1, filesize, 
-					groups, hits, created_by, checked_out, checked_out_time, 
-					ordering, access, show_unauth_links, redirect_unauth, 
-					published, language, environments  
-				FROM jos_ars_items WHERE id=' . $itemId );
-		while ( $row = $stmt->fetch_array ( MYSQL_ASSOC ) ) {
-			$item = $row;
-		}
-	
-		header ( 'Content-type: application/json' );
-		echo json_encode ( $item );
-		$stmt->close ();
-	}
-	
-	function getItemDetail($itemId) {
-		// prepare the statement
-		$stmt = $this->db->query (
-				'SELECT 
-				i.id, i.release_id, i.title, i.alias, i.description, i.type,
-				i.filename, i.url, i.updatestream, i.md5, i.sha1, i.filesize,
-				i.groups, i.hits, i.created_by, i.checked_out, i.checked_out_time,
-				i.ordering, i.access, i.show_unauth_links, i.redirect_unauth,
-				i.published, i.language, i.environments,
-				r.id, r.category_id, r.version, r.alias, r.maturity, r.description, 
-				r.notes, r.groups, r.hits, r.created, r.created_by, r.modified, 
-				r.modified_by, r.checked_out, r.checked_out_time, r.ordering, 
-				r.access, r.show_unauth_links, r.redirect_unauth, r.published, 
-				r.language,
-				c.id, c.title, c.alias, c.description, c.type, c.groups, c.directory, 
-				c.vgroup_id, c.created, c.created_by, c.modified, c.modified_by,
-				c.checked_out, c.checked_out_time, c.ordering, c.access, c.show_unauth_links,
-				c.redirect_unauth, c.published, c.language
-			FROM 
-				jos_ars_items i
-			INNER JOIN
-				jos_ars_releases r on i.release_id = r.id
-			INNER JOIN
-				jos_ars_categories c on r.category_id = c.id
-			WHERE 
-				i.id=' . $itemId );
-		while ( $row = $stmt->fetch_array ( MYSQL_ASSOC ) ) {
-			$item = $row;
-		}
-	
-		header ( 'Content-type: application/json' );
-		echo json_encode ( $item );
-		$stmt->close ();
-	}
-*/	
-
 	// Get all the items and their subcomponents
 	function getAllItemsRest() {
 		// prepare the items statement
@@ -173,7 +104,7 @@ class ArsService extends DBConfig {
 			LEFT JOIN
 				jos_akeeba_item_custom c ON i.id = c.item_id
 			INNER JOIN
-				jos_ars_releases r ON i.release_id = r.id;
+				jos_ars_releases r ON i.release_id = r.id
 			ORDER BY i.title');
 		$counter = 0;
 
@@ -184,7 +115,8 @@ class ArsService extends DBConfig {
 			
 			// prepare the RELEASE statement
  			$stmt2 = $this->db->query (
- 					'SELECT * FROM jos_ars_releases WHERE id=' . $releaseId );
+				'SELECT * FROM jos_ars_releases 
+				WHERE id=' . $releaseId );
 
 			// should only return one result.  put that row in a variable
 			while ( $row2 = $stmt2->fetch_array ( MYSQL_ASSOC ) ) {
@@ -193,7 +125,8 @@ class ArsService extends DBConfig {
 
 				// prepare the CATEGORY statement
 				$stmt3 = $this->db->query (
-						'SELECT *FROM jos_ars_categories WHERE id=' . $categoryId );
+					'SELECT * FROM jos_ars_categories 
+					WHERE id=' . $categoryId );
  			
 				// should only return one result.  put that row in a variable
 				while ( $row3 = $stmt3->fetch_array ( MYSQL_ASSOC ) ) {
@@ -230,12 +163,14 @@ class ArsService extends DBConfig {
 				i.published, i.language, i.environments,
 				c.icon_url, c.short_description, c.discussion_url, c.issues_url,
 				d.documentation_link, d.documentation_text
-			FROM jos_ars_items i 
+			FROM 
+				jos_ars_items i 
 			LEFT JOIN
 				jos_akeeba_item_custom c ON i.id = c.item_id
 			LEFT JOIN
 				jos_akeeba_item_documentation d ON i.id = d.item_id
-			WHERE i.id=' . $itemId);
+			WHERE 
+				i.id=' . $itemId);
 		
 		// should only return one result.  put that row in a variable
 		while ( $row = $stmt->fetch_array ( MYSQL_ASSOC ) ) {
@@ -245,8 +180,8 @@ class ArsService extends DBConfig {
 		
 		// prepare the statement for the RELEASE
 		$stmt2 = $this->db->query (
-				'SELECT * FROM jos_ars_releases
-				WHERE id=' . $releaseId );
+			'SELECT * FROM jos_ars_releases
+			WHERE id=' . $releaseId );
 
 		// should only return one result.  put that row in a variable
 		while ( $row2 = $stmt2->fetch_array ( MYSQL_ASSOC ) ) {
@@ -296,7 +231,7 @@ class ArsService extends DBConfig {
 			FROM 
 				jos_ars_items i
 			INNER JOIN
-				jos_ars_releases r on i.release_id = r.id
+				jos_ars_releases r ON i.release_id = r.id
 			WHERE 
 				r.category_id=' . $catId);
 		$counter = 0;
@@ -309,7 +244,8 @@ class ArsService extends DBConfig {
 				
 			// prepare the statement for RELEASE
 			$stmt2 = $this->db->query (
-					'SELECT * FROM jos_ars_releases WHERE id=' . $releaseId );
+				'SELECT * FROM jos_ars_releases 
+				WHERE id=' . $releaseId );
 			
 			// should only return one result.  put that row in a variable
 			while ( $row2 = $stmt2->fetch_array ( MYSQL_ASSOC ) ) {
@@ -318,7 +254,8 @@ class ArsService extends DBConfig {
 
 				// prepare the statement for CATEGORY
 				$stmt3 = $this->db->query (
-						'SELECT *FROM jos_ars_categories WHERE id=' . $categoryId );
+					'SELECT * FROM jos_ars_categories 
+					WHERE id=' . $categoryId );
 
 				// should only return one result.  put that row in a variable
 				while ( $row3 = $stmt3->fetch_array ( MYSQL_ASSOC ) ) {
@@ -374,7 +311,8 @@ class ArsService extends DBConfig {
 	
 			// prepare the statement for RELEASE
 			$stmt2 = $this->db->query (
-				'SELECT * FROM jos_ars_releases WHERE id=' . $releaseId );
+				'SELECT * FROM jos_ars_releases 
+				WHERE id=' . $releaseId );
 			
 			// should only return one result.  put that row in a variable
 			while ( $row2 = $stmt2->fetch_array ( MYSQL_ASSOC ) ) {
@@ -383,7 +321,8 @@ class ArsService extends DBConfig {
 
 				// prepare the statement for CATEGORY
 				$stmt3 = $this->db->query (
-					'SELECT *FROM jos_ars_categories WHERE id=' . $categoryId );
+					'SELECT * FROM jos_ars_categories 
+					WHERE id=' . $categoryId );
 
 				// should only return one result.  put that row in a variable
 				while ( $row3 = $stmt3->fetch_array ( MYSQL_ASSOC ) ) {
@@ -414,7 +353,8 @@ class ArsService extends DBConfig {
 	// this is mostly just for reference use getAllCategories always
 	function getAllCategoriesArray() {
 		$stmt = $this->db->prepare (
-			'SELECT id, title FROM jos_ars_categories' );
+			'SELECT id, title 
+			FROM jos_ars_categories' );
 		$stmt->execute ();
 		$stmt->bind_result ( $id, $title );
 		while ( $stmt->fetch () ) {
@@ -438,13 +378,9 @@ class ArsService extends DBConfig {
 			FROM 
 				jos_ars_items i
 			INNER JOIN 
-				jos_ars_releases r
-			ON 
-				i.release_id = r.id
+				jos_ars_releases r ON i.release_id = r.id
 			INNER JOIN 
-				jos_ars_categories c
-			ON 
-				r.category_id = c.id
+				jos_ars_categories c ON r.category_id = c.id
 			WHERE 
 				i.id = ' . $itemId );
 		while ( $row = $stmt->fetch_array ( MYSQL_ASSOC ) ) {
@@ -458,31 +394,23 @@ class ArsService extends DBConfig {
 	function getItemFileName($itemId) {
 		// prepare the statement
 		$stmt = $this->db->query ( 
-			'SELECT 
-				i.filename 
-			FROM 
-				jos_ars_items i 
-			WHERE 
-				i.id = ' . $itemId );
+			'SELECT i.filename 
+			FROM jos_ars_items i 
+			WHERE i.id = ' . $itemId );
 		while ( $row = $stmt->fetch_array ( MYSQL_ASSOC ) ) {
 			$arrItem = $row;
 		}
 
 		$stmt->close();
-//		incrementItemHitCount($itemId);
-		
 		return $arrItem;
 	}
 
 	function incrementItemHitCount($itemId) {
 		// prepare the statement
 		$stmt = $this->db->prepare ( 
-			'UPDATE 
-				jos_ars_items 
-			SET 
-				hits = hits + 1 
-			WHERE 
-				id = ' . $itemId );
+			'UPDATE jos_ars_items 
+			SET hits = hits + 1 
+			WHERE id = ' . $itemId );
 		$stmt->execute ();
 		$stmt->close ();
 	}
