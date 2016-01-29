c6ff8ac20d67fac014a04204ae8f1b459c535b19
Robert <rothattack@gmail.com>
2016-01-29 14:56:58 -0600

    modified into a customizable list of links

--- mod_leidos_research_tools/mod_leidos_research_tools.php
+++ mod_leidos_research_tools/mod_leidos_research_tools.php
@@ -10,7 +10,17 @@ defined('_JEXEC') or die;
 // get our module layout, for now we're using the default
 $layout = $params->get('layout', 'default');
 // get the header text from user input
-$research_header = $params->get('header', 'Resource');
+$research_header = $params->get('header');
+
+// create the array of Links
+$arrLink = [];
+for ($i=0; $i < 8; $i++) { 
+	$arrLink[$i] = array(
+		'name' => $params->get('link'. ($i + 1) . '_name'),
+		'url' => $params->get('link'. ($i + 1) . '_url'),
+		'icon' => $params->get('link'. ($i + 1) . '_icon', 'link')
+		);
+}
 // user
 $user = JFactory::getUser();
 // require our helper class OsadpResearch
