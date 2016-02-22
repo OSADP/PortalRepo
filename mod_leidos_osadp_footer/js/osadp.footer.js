79c28448b4d01a4f3f8cf62746365df478037d04
Robert <rothattack@gmail.com>
2016-02-22 09:35:21 -0600

    create a custom event for force toggle

--- mod_leidos_osadp_footer/js/osadp.footer.js
+++ mod_leidos_osadp_footer/js/osadp.footer.js
@@ -8,16 +8,18 @@
 		var $element = $('.osadp-footer');
 		// detects modifications to our DOM e.g. Asynchronous DOM changes
 		$('document').bind("DOMSubtreeModified", toggleFooter);
+		$element.on('OSADPFooterToggle', toggleFooter);
 		// run when footer loads in our DOM
-		$element.ready(function() { 
+		$element.ready( function() { 
 			toggleFooter();
 		});
 		// detect scroll, might need requestFra
 		$(window).scroll(function() {
 			setTimeout(toggleFooter, 100);
 		});
-
+		// function to show/hide the footer
 		function toggleFooter() {
+			console.info('Info: Footer toggled.');
 			if($(window).scrollTop() + $(window).height() + 80 >= $(document).height()) {
 	 			$element.show();
 	   	} else {
