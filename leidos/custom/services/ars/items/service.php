c6f3158a191e4b87e79a693857939f57cd843ad5
Rob Roth <rothattack@gmail.com>
2016-03-29 10:13:59 -0500

    removed utf8 parsing

--- leidos/custom/services/ars/items/service.php
+++ leidos/custom/services/ars/items/service.php
@@ -1,11 +1,8 @@
 <?php
 	require (dirname ( __DIR__ ) . "/items/implementation.php");
-	require (dirname ( __DIR__ ) . "/../services/utf8parser.php");
 	require (dirname ( __DIR__ ) . "/../services/rest.php");
 	// implementation class
 	$implementation = new ArsItems();
-	// utf8 parser class
-	$utf8parser = new UTF8parser();
 	// REST class
 	$router = new RouteConfig();
 
@@ -16,10 +13,10 @@
 		// get release by id if available, else get all items
 		if ( $child = $router->child('items') ) {
 			$rel = $implementation->get( $child );
-			echo json_encode ( $utf8parser->convert( $rel ) );
+			echo json_encode ( $rel );
 		} else {
 			$arrRel = $implementation->all();
-			echo json_encode ( $utf8parser->convert( $arrRel ) );
+			echo json_encode ( $arrRel );
 		}
 	}
 ?>
