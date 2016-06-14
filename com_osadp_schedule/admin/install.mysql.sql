5f2b3dc8416bcd340e93f4ad2c1b554466e6410e
Rob Roth <rothattack@gmail.com>
2016-06-14 09:03:57 -0500

    updated the create table query

--- com_osadp_schedule/admin/install.mysql.sql
+++ com_osadp_schedule/admin/install.mysql.sql
@@ -1,8 +1,18 @@
 -- create our main table for each project
-CREATE TABLE IF NOT EXISTS `#__osadp_release_schedule` (
+CREATE TABLE IF NOT EXISTS `#_osadp_release_schedule` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `notes` text DEFAULT NULL,
-  `dma` tinyint(1) DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `notes` text COLLATE utf8_unicode_ci,
+  `dma` tinyint(1) NOT NULL,
+  `date` datetime NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime DEFAULT NULL,
+  `created_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `published` tinyint(1) NOT NULL DEFAULT '0',
+  `available` tinyint(1) NOT NULL DEFAULT '0',
+  `last_modified_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT '',
+  `capabilities` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+  `full_date` tinyint(1) DEFAULT '1',
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT '',
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
